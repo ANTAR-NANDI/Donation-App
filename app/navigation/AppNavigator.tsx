4fd0710b56316b1d15228220e20765f2f7cd7051
ANTAR-NANDI <antarnandy.2@gmail.com>
2025-06-04 18:16:18 +0600

    payment ready view done

--- app/navigation/AppNavigator.tsx
+++ app/navigation/AppNavigator.tsx
@@ -1,35 +1,35 @@
 // RootStackNavigator.js or App.js
-import React, { useState, useEffect } from 'react';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { createStackNavigator } from '@react-navigation/stack';
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import { Ionicons } from '@expo/vector-icons';
-import { TouchableOpacity } from 'react-native';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { I18nextProvider } from 'react-i18next';
-import i18n from '../../i18n';
-import HomeScreen from '../screens/HomeScreen';
-import MemberRegistrationScreen from '../screens/MemberRegistrationScreen';
-import ProfileScreen from '../screens/ProfileScreen';
-import LoginScreen from '../screens/LoginScreen';
-import RegistrationScreen from '../screens/RegistrationScreen';
-import Index from '../screens/Index';
-import DonationScreen from '../screens/DonationScreen';
-import NotificationScreen from '../screens/NotificationScreen';
-import ChatScreen from '../screens/ChatScreen';
-import LogoutScreen from '../screens/LogoutScreen';
-import NotificationDetailsScreen from '../screens/NotificationDetailsScreen';
-import NewsDetailScreen from '../screens/NewsDetailsScreen';
-import NewsScreen from '../screens/NewsScreen';
-import PronamiScreen from '../screens/PronamiScreen';
-import AddPronamiScreen from '../screens/AddPronamiScreen';
-import DevoteeScreen from '../screens/DevoteeScreen';
-import DevoteeDetailScreen from '../screens/DevoteeDetailScreen';
-import ForgetPasswordScreen from '../screens/ForgetPasswordScreen';
-import ResetPasswordScreen from '../screens/ResetPasswordScreen';
-import NotificationProvider from '../screens/Notification-Provider';
-import EditDevoteeScreen from '../screens/EditDevoteeScreen';
-
+import React, { useState, useEffect } from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createStackNavigator } from "@react-navigation/stack";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { Ionicons } from "@expo/vector-icons";
+import { TouchableOpacity } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { I18nextProvider } from "react-i18next";
+import i18n from "../../i18n";
+import HomeScreen from "../screens/HomeScreen";
+import MemberRegistrationScreen from "../screens/MemberRegistrationScreen";
+import ProfileScreen from "../screens/ProfileScreen";
+import LoginScreen from "../screens/LoginScreen";
+import RegistrationScreen from "../screens/RegistrationScreen";
+import Index from "../screens/Index";
+import DonationScreen from "../screens/DonationScreen";
+import NotificationScreen from "../screens/NotificationScreen";
+import ChatScreen from "../screens/ChatScreen";
+import LogoutScreen from "../screens/LogoutScreen";
+import NotificationDetailsScreen from "../screens/NotificationDetailsScreen";
+import NewsDetailScreen from "../screens/NewsDetailsScreen";
+import NewsScreen from "../screens/NewsScreen";
+import PronamiScreen from "../screens/PronamiScreen";
+import AddPronamiScreen from "../screens/AddPronamiScreen";
+import DevoteeScreen from "../screens/DevoteeScreen";
+import DevoteeDetailScreen from "../screens/DevoteeDetailScreen";
+import ForgetPasswordScreen from "../screens/ForgetPasswordScreen";
+import ResetPasswordScreen from "../screens/ResetPasswordScreen";
+import NotificationProvider from "../screens/Notification-Provider";
+import EditDevoteeScreen from "../screens/EditDevoteeScreen";
+import PaymentDetailsScreen from "../screens/PaymentDetailsScreen";
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
 const Drawer = createDrawerNavigator();
@@ -37,7 +37,10 @@ const Drawer = createDrawerNavigator();
 const getScreenOptions = (navigation, title) => ({
   title: title,
   headerLeft: () => (
-    <TouchableOpacity onPress={() => navigation.openDrawer()} style={{ marginLeft: 15 }}>
+    <TouchableOpacity
+      onPress={() => navigation.openDrawer()}
+      style={{ marginLeft: 15 }}
+    >
       <Ionicons name="menu-outline" size={28} color="black" />
     </TouchableOpacity>
   ),
@@ -45,39 +48,100 @@ const getScreenOptions = (navigation, title) => ({
 
 const DashboardStackNavigator = ({ navigation }) => (
   <Stack.Navigator>
-    <Stack.Screen name="Home" component={HomeScreen} options={getScreenOptions(navigation, 'Home')} />
-    <Stack.Screen name="MemberRegistration" component={MemberRegistrationScreen} options={getScreenOptions(navigation, 'Member Registration')} />
+    <Stack.Screen
+      name="Home"
+      component={HomeScreen}
+      options={getScreenOptions(navigation, "Home")}
+    />
+    <Stack.Screen
+      name="MemberRegistration"
+      component={MemberRegistrationScreen}
+      options={getScreenOptions(navigation, "Member Registration")}
+    />
   </Stack.Navigator>
 );
 
 const DevoteeStackNavigator = ({ navigation }) => (
   <Stack.Navigator>
-    <Stack.Screen name="AllDevotees" component={DevoteeScreen} options={{ title: 'All Devotees' }} />
-    <Stack.Screen name="DevoteeDetail" component={DevoteeDetailScreen} options={{ headerShown: false }} />
-    <Stack.Screen name="EditDevotee" component={EditDevoteeScreen} options={{ headerShown: false }} />
-    <Stack.Screen name="MemberRegistration" component={MemberRegistrationScreen} options={{ headerShown: false }} />
+    <Stack.Screen
+      name="AllDevotees"
+      component={DevoteeScreen}
+      options={{ title: "All Devotees" }}
+    />
+    <Stack.Screen
+      name="DevoteeDetail"
+      component={DevoteeDetailScreen}
+      options={{ headerShown: false }}
+    />
+    <Stack.Screen
+      name="EditDevotee"
+      component={EditDevoteeScreen}
+      options={{ headerShown: false }}
+    />
+    <Stack.Screen
+      name="MemberRegistration"
+      component={MemberRegistrationScreen}
+      options={{ headerShown: false }}
+    />
   </Stack.Navigator>
 );
 
 const PronamiStackNavigator = ({ navigation }) => (
   <Stack.Navigator>
-    <Stack.Screen name="AllPronamis" component={PronamiScreen} options={{ title: 'All Pronamis' }} />
-    <Stack.Screen name="AddPronami" component={AddPronamiScreen} options={{ headerShown: false }} />
+    <Stack.Screen
+      name="AllPronamis"
+      component={PronamiScreen}
+      options={{ title: "All Pronamis" }}
+    />
+    <Stack.Screen
+      name="AddPronami"
+      component={AddPronamiScreen}
+      options={{ headerShown: false }}
+    />
   </Stack.Navigator>
 );
 
-
 const NewsStackNavigator = ({ navigation }) => (
   <Stack.Navigator>
-    <Stack.Screen name="News" component={NewsScreen} options={{ title: 'All News' }} />
-    <Stack.Screen name="NewsDetail" component={NewsDetailScreen} options={{ headerShown: false }} />
+    <Stack.Screen
+      name="News"
+      component={NewsScreen}
+      options={{ title: "All News" }}
+    />
+    <Stack.Screen
+      name="NewsDetail"
+      component={NewsDetailScreen}
+      options={{ headerShown: false }}
+    />
   </Stack.Navigator>
 );
 
 const NotificationStackNavigator = ({ navigation }) => (
   <Stack.Navigator>
-    <Stack.Screen name="Notification" component={NotificationScreen} options={getScreenOptions(navigation, 'Notifications')} />
-    <Stack.Screen name="NotificationDetail" component={NotificationDetailsScreen} options={{ title: 'Notification Details' }} />
+    <Stack.Screen
+      name="Notification"
+      component={NotificationScreen}
+      options={getScreenOptions(navigation, "Notifications")}
+    />
+    <Stack.Screen
+      name="NotificationDetail"
+      component={NotificationDetailsScreen}
+      options={{ title: "Notification Details" }}
+    />
+  </Stack.Navigator>
+);
+const DonationStackNavigator = ({ navigation }) => (
+  <Stack.Navigator>
+    <Stack.Screen
+      name="Donation"
+      component={DonationScreen}
+      options={getScreenOptions(navigation, "Donation")}
+    />
+    <Stack.Screen
+      name="DonationDetail"
+      component={PaymentDetailsScreen}
+      options={{ title: "Payment Details" }}
+    />
   </Stack.Navigator>
 );
 
@@ -88,21 +152,28 @@ const TabNavigator = ({ navigation }) => (
       component={DashboardStackNavigator}
       options={{
         headerShown: false,
-        tabBarIcon: ({ color, size }) => <Ionicons name="home-outline" size={size} color={color} />,
+        tabBarIcon: ({ color, size }) => (
+          <Ionicons name="home-outline" size={size} color={color} />
+        ),
       }}
     />
     <Tab.Screen
       name="Chat"
       component={ChatScreen}
       options={{
-        tabBarIcon: ({ color, size }) => <Ionicons name="chatbubbles-outline" size={size} color={color} />,
+        tabBarIcon: ({ color, size }) => (
+          <Ionicons name="chatbubbles-outline" size={size} color={color} />
+        ),
       }}
     />
     <Tab.Screen
       name="Donation"
-      component={DonationScreen}
+      component={DonationStackNavigator}
       options={{
-        tabBarIcon: ({ color, size }) => <Ionicons name="heart-outline" size={size} color={color} />,
+        headerShown: false,
+        tabBarIcon: ({ color, size }) => (
+          <Ionicons name="notifications-outline" size={size} color={color} />
+        ),
       }}
     />
     <Tab.Screen
@@ -110,14 +181,18 @@ const TabNavigator = ({ navigation }) => (
       component={NotificationStackNavigator}
       options={{
         headerShown: false,
-        tabBarIcon: ({ color, size }) => <Ionicons name="notifications-outline" size={size} color={color} />,
+        tabBarIcon: ({ color, size }) => (
+          <Ionicons name="notifications-outline" size={size} color={color} />
+        ),
       }}
     />
     <Tab.Screen
       name="Profile"
       component={ProfileScreen}
       options={{
-        tabBarIcon: ({ color, size }) => <Ionicons name="person-outline" size={size} color={color} />,
+        tabBarIcon: ({ color, size }) => (
+          <Ionicons name="person-outline" size={size} color={color} />
+        ),
       }}
     />
   </Tab.Navigator>
@@ -125,22 +200,92 @@ const TabNavigator = ({ navigation }) => (
 
 const DrawerNavigator = () => (
   <Drawer.Navigator>
-    <Drawer.Screen name="Dashboard" component={TabNavigator} options={{ headerShown: false, drawerIcon: () => <Ionicons name="home-outline" size={24} color="black" /> }} />
-    <Drawer.Screen name="Profile" component={ProfileScreen} options={{ drawerIcon: () => <Ionicons name="person-outline" size={24} color="black" /> }} />
-    <Drawer.Screen name="Chat" component={ChatScreen} options={{ drawerIcon: () => <Ionicons name="chatbubbles-outline" size={24} color="black" /> }} />
-    <Drawer.Screen name="News" component={NewsStackNavigator} options={{ drawerIcon: () => <Ionicons name="notifications-outline" size={24} color="black" /> }} />
-    <Drawer.Screen name="Pronami" component={PronamiStackNavigator} options={{ drawerIcon: () => <Ionicons name="heart-outline" size={24} color="black" /> }} />
-    <Drawer.Screen name="Devotees" component={DevoteeStackNavigator} options={{ drawerIcon: () => <Ionicons name="person-outline" size={24} color="black" /> }} />
-    <Drawer.Screen name="Logout" component={LogoutScreen} options={{ headerShown: false, drawerIcon: () => <Ionicons name="log-out-outline" size={24} color="black" /> }} />
+    <Drawer.Screen
+      name="Dashboard"
+      component={TabNavigator}
+      options={{
+        headerShown: false,
+        drawerIcon: () => (
+          <Ionicons name="home-outline" size={24} color="black" />
+        ),
+      }}
+    />
+    <Drawer.Screen
+      name="Profile"
+      component={ProfileScreen}
+      options={{
+        drawerIcon: () => (
+          <Ionicons name="person-outline" size={24} color="black" />
+        ),
+      }}
+    />
+    <Drawer.Screen
+      name="Chat"
+      component={ChatScreen}
+      options={{
+        drawerIcon: () => (
+          <Ionicons name="chatbubbles-outline" size={24} color="black" />
+        ),
+      }}
+    />
+    <Drawer.Screen
+      name="News"
+      component={NewsStackNavigator}
+      options={{
+        drawerIcon: () => (
+          <Ionicons name="notifications-outline" size={24} color="black" />
+        ),
+      }}
+    />
+    <Drawer.Screen
+      name="Pronami"
+      component={PronamiStackNavigator}
+      options={{
+        drawerIcon: () => (
+          <Ionicons name="heart-outline" size={24} color="black" />
+        ),
+      }}
+    />
+    <Drawer.Screen
+      name="Devotees"
+      component={DevoteeStackNavigator}
+      options={{
+        drawerIcon: () => (
+          <Ionicons name="person-outline" size={24} color="black" />
+        ),
+      }}
+    />
+    <Drawer.Screen
+      name="Logout"
+      component={LogoutScreen}
+      options={{
+        headerShown: false,
+        drawerIcon: () => (
+          <Ionicons name="log-out-outline" size={24} color="black" />
+        ),
+      }}
+    />
   </Drawer.Navigator>
 );
 
 const AuthStack = () => (
   <Stack.Navigator initialRouteName="Index">
-    <Stack.Screen name="Index" component={Index} options={{ title: 'Home' }} />
-    <Stack.Screen name="Login" component={LoginScreen} options={{ title: 'Login' }} />
-    <Stack.Screen name="ForgetPassword" component={ForgetPasswordScreen} options={{ title: 'Forget Password' }} />
-    <Stack.Screen name="ResetPassword" component={ResetPasswordScreen} options={{ title: 'Reset Password' }} />
+    <Stack.Screen name="Index" component={Index} options={{ title: "Home" }} />
+    <Stack.Screen
+      name="Login"
+      component={LoginScreen}
+      options={{ title: "Login" }}
+    />
+    <Stack.Screen
+      name="ForgetPassword"
+      component={ForgetPasswordScreen}
+      options={{ title: "Forget Password" }}
+    />
+    <Stack.Screen
+      name="ResetPassword"
+      component={ResetPasswordScreen}
+      options={{ title: "Reset Password" }}
+    />
     <Stack.Screen name="Register" component={RegistrationScreen} />
   </Stack.Navigator>
 );
@@ -150,7 +295,7 @@ const RootStackNavigator = () => {
 
   useEffect(() => {
     const checkAuthToken = async () => {
-      const token = await AsyncStorage.getItem('@auth_token');
+      const token = await AsyncStorage.getItem("@auth_token");
       setIsAuthenticated(!!token);
     };
     checkAuthToken();
@@ -161,9 +306,17 @@ const RootStackNavigator = () => {
   return (
     <I18nextProvider i18n={i18n}>
       <NotificationProvider>
-        <Stack.Navigator initialRouteName={isAuthenticated ? 'App' : 'Auth'}>
-          <Stack.Screen name="Auth" component={AuthStack} options={{ headerShown: false }} />
-          <Stack.Screen name="App" component={DrawerNavigator} options={{ headerShown: false }} />
+        <Stack.Navigator initialRouteName={isAuthenticated ? "App" : "Auth"}>
+          <Stack.Screen
+            name="Auth"
+            component={AuthStack}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="App"
+            component={DrawerNavigator}
+            options={{ headerShown: false }}
+          />
         </Stack.Navigator>
       </NotificationProvider>
     </I18nextProvider>
--- app/screens/DonationScreen.tsx
+++ app/screens/DonationScreen.tsx
@@ -1,103 +1,43 @@
-import React, { useState } from 'react';
-import { useRouter } from 'expo-router';
-import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
-import axios from 'axios';
-import Toast from 'react-native-toast-message';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import RNPickerSelect from 'react-native-picker-select';
-import BASE_URL from '@/config';
-import { useTranslation } from 'react-i18next';
+import React from "react";
+import { View, Text, TouchableOpacity, StyleSheet, Image } from "react-native";
+import { useRouter } from "expo-router"; // or use navigation.navigate if using react-navigation
+import { useTranslation } from "react-i18next";
+
 const DonationScreen = ({ navigation }: any) => {
-    const { t, i18n } = useTranslation();
-  // State to store form inputs
-  const [name, setName] = useState('');
-  const [amount, setAmount] = useState('');
-  const [method, setPaymentMethod] = useState('');
+  const { t } = useTranslation();
 
-  // Form validation
-  const validateForm = () => {
-    if (!name.trim()) {
-      Toast.show({ type: 'error', text1: 'Validation Error', text2: 'Name is required.' });
-      return false;
-    }
-    if (!amount.trim()) {
-      Toast.show({ type: 'error', text1: 'Validation Error', text2: 'Amount is required.' });
-      return false;
-    }
-    if (!method) {
-      Toast.show({ type: 'error', text1: 'Validation Error', text2: 'Please select a payment method.' });
-      return false;
-    }
-    return true;
-  };
-
-  // Handle registration logic
-  const handleRegister = async () => {
-    if (!validateForm()) return;
-     try {
-      
-            const response = await axios.post(`${BASE_URL}/donation`, 
-              { 
-             name,
-             amount,
-             method, 
-             },
-              {
-                headers: {
-                  Authorization: `Bearer ${await AsyncStorage.getItem("@auth_token")}`,
-                  "Content-Type": "application/json", // Ensure correct content type
-                },
-               });
-              Toast.show({ type: 'success', text1: 'Success', text2: 'Donation Successful!' });
-          navigation.replace('App');
-
-          } catch (error) {
-            Toast.show({ type: 'error', text1: 'Validation Error', text2: error.response.data.error });
-          }
+  const handlePaymentSelection = (method: string) => {
+    // Navigate to the next screen with selected method
+    navigation.navigate("DonationDetail", { method }); // adjust the screen name
   };
 
   return (
     <View style={styles.container}>
-      <Text style={styles.heading}>{t('donation_form')}</Text>
+      <Text style={styles.label}>{t("select_payment_method")}</Text>
 
-      <Text style={styles.label}>{t('name')} *</Text>
-      <TextInput
-        style={styles.input}
-        placeholder={t('name_placeholder')}
-        placeholderTextColor="#4D2600"
-        value={name}
-        onChangeText={setName}
-      />
+      <View style={styles.paymentContainer}>
+        <TouchableOpacity
+          style={styles.paymentBox}
+          onPress={() => handlePaymentSelection("nagad")}
+        >
+          <Image
+            source={require("../../assets/images/nagad.jpg")}
+            style={styles.image}
+          />
+          <Text style={styles.paymentText}>Nagad</Text>
+        </TouchableOpacity>
 
-      <Text style={styles.label}>{t('donation_amount')} *</Text>
-      <TextInput
-        style={styles.input}
-        placeholder={t('donation_amount_placeholder')}
-        placeholderTextColor="#4D2600"
-        value={amount}
-        onChangeText={setAmount}
-        keyboardType="numeric"
-      />
-
-      <Text style={styles.label}>{t('select_payment_method')} *</Text>
-      <View style={styles.pickerContainer}>
-        <RNPickerSelect
-          onValueChange={(value) => setPaymentMethod(value)}
-          items={[
-            { label: 'Credit Card', value: 'credit_card' },
-            { label: 'PayPal', value: 'paypal' },
-            { label: 'Bank Transfer', value: 'bank_transfer' },
-          ]}
-        />
+        <TouchableOpacity
+          style={styles.paymentBox}
+          onPress={() => handlePaymentSelection("rocket")}
+        >
+          <Image
+            source={require("../../assets/images/rocket.jpg")}
+            style={styles.image}
+          />
+          <Text style={styles.paymentText}>Rocket</Text>
+        </TouchableOpacity>
       </View>
-
-      
-
-      <TouchableOpacity style={styles.registerButton} onPress={handleRegister}>
-        <Text style={styles.buttonText}>{t('donate')}</Text>
-      </TouchableOpacity>
-
-      <Toast />
     </View>
   );
 };
@@ -105,82 +45,56 @@ const DonationScreen = ({ navigation }: any) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#D5C295',
+    backgroundColor: "#D5C295",
     padding: 20,
-    justifyContent: 'center',
-  },
-  heading: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    marginBottom: 20,
-    color: '#4D2600',
+    justifyContent: "center",
   },
   label: {
     fontSize: 16,
-    fontWeight: 'bold',
-    color: '#4D2600',
+    fontWeight: "bold",
+    color: "#4D2600",
+    marginBottom: 15,
+    textAlign: "center",
+  },
+  paymentContainer: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    marginBottom: 30,
+  },
+  paymentBox: {
+    backgroundColor: "#C7A977",
+    padding: 10,
+    borderRadius: 15,
+    alignItems: "center",
+    width: 150,
+    height: 100,
+    justifyContent: "center",
+  },
+  image: {
+    width: 80,
+    height: 70,
+    resizeMode: "contain",
     marginBottom: 5,
   },
-  input: {
-    borderWidth: 2,
-    borderColor: '#4D2600',
-    borderRadius: 25,
-    paddingHorizontal: 15,
-    paddingVertical: 8,
-    fontSize: 16,
-    color: '#4D2600',
-    marginBottom: 15,
-    backgroundColor: '#FFF',
-  },
-  pickerContainer: {
-    borderRadius: 25,
-    backgroundColor: '#FFF',
-    paddingHorizontal: 10,
-    paddingVertical: 5,
-    marginBottom: 15,
-  },
-  selectedText: {
-    marginTop: 10,
-    fontSize: 16,
-    fontWeight: 'bold',
-    color: '#4D2600',
+  paymentText: {
+    color: "#4D2600",
+    fontWeight: "bold",
+    textAlign: "center",
+    fontSize: 12,
   },
   registerButton: {
-    backgroundColor: '#4D2600',
+    backgroundColor: "#4D2600",
     paddingVertical: 12,
     borderRadius: 25,
-    alignItems: 'center',
+    alignItems: "center",
     marginTop: 10,
+    marginHorizontal: 60,
   },
   buttonText: {
     fontSize: 18,
-    color: '#FFFFFF',
-    fontWeight: 'bold',
+    color: "#FFFFFF",
+    fontWeight: "bold",
   },
 });
-
-const pickerSelectStyles = {
-  inputIOS: {
-    fontSize: 16,
-    paddingVertical: 12,
-    paddingHorizontal: 15,
-    borderWidth: 0, // Removed the border
-    borderRadius: 25,
-    backgroundColor: '#FFF',
-    color: '#4D2600',
-    paddingRight: 30, // Prevents text from cutting off
-  },
-  inputAndroid: {
-    fontSize: 16,
-    paddingVertical: 12,
-    paddingHorizontal: 15,
-    borderWidth: 0, // Removed the border
-    borderRadius: 25,
-    backgroundColor: '#FFF',
-    color: '#4D2600',
-    paddingRight: 30, // Prevents text from cutting off
-  },
-};
 
 export default DonationScreen;
